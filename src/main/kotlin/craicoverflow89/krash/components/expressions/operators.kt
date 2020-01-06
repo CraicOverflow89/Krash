8e2df5403713f1fd48e4f712e1856e57c9297184
CraicOverflow89 <craic.overflow@hotmail.com>
2020-01-06 21:33:03 +0000

    Finished first draft of operator classes.

--- src/main/kotlin/craicoverflow89/krash/components/expressions/operators.kt
+++ src/main/kotlin/craicoverflow89/krash/components/expressions/operators.kt
@@ -2,34 +2,110 @@ package craicoverflow89.krash.components.expressions
 
 import craicoverflow89.krash.components.KrashRuntime
 import craicoverflow89.krash.components.objects.KrashValueInteger
+import craicoverflow89.krash.components.objects.KrashValueSimple
+import craicoverflow89.krash.components.objects.KrashValueString
 
-abstract class KrashExpressionOperator: KrashExpression()
+abstract class KrashExpressionOperator(private val first: KrashExpression, private val second: KrashExpression): KrashExpression() {
 
-class KrashExpressionOperatorAddition(private val first: Int, private val second: Int): KrashExpressionOperator() {
+    abstract fun invoke(first: KrashValueSimple, second: KrashValueSimple): KrashValueSimple
 
-    override fun toValue(runtime: KrashRuntime) =
-        KrashValueInteger(first + second)
-
-}
-
-class KrashExpressionOperatorDivision(private val first: Int, private val second: Int): KrashExpressionOperator() {
-
-    override fun toValue(runtime: KrashRuntime) =
-        KrashValueInteger(first / second)
-    // NOTE: should probably add custom validation to prevent division by zero issues
+    override fun toValue(runtime: KrashRuntime) = invoke(first.toValue(runtime), second.toValue(runtime))
 
 }
 
-class KrashExpressionOperatorMultiplication(private val first: Int, private val second: Int): KrashExpressionOperator() {
+class KrashExpressionOperatorAddition(first: KrashExpression, second: KrashExpression): KrashExpressionOperator(first, second) {
 
-    override fun toValue(runtime: KrashRuntime) =
-        KrashValueInteger(first * second)
+    override fun invoke(first: KrashValueSimple, second: KrashValueSimple) = when(first) {
+
+        // Integer Addition
+        is KrashValueInteger -> {
+
+            // Perform Addition
+            if(second is KrashValueInteger) KrashValueInteger(first.value + second.value)
+
+            // Invalid Type
+            else throw RuntimeException("Invalid type to perform addition!")
+        }
+
+        // String Concatenation
+        is KrashValueString -> {
+
+            // Perform Concatenation
+            if(second is KrashValueString) KrashValueString(first.value + second.value)
+
+            // Invalid Type
+            else throw RuntimeException("Invalid type to perform concatenation!")
+        }
+
+        // Invalid Type
+        else -> throw RuntimeException("Invalid type to perform operator!")
+    }
 
 }
 
-class KrashExpressionOperatorSubtraction(private val first: Int, private val second: Int): KrashExpressionOperator() {
+class KrashExpressionOperatorDivision(first: KrashExpression, second: KrashExpression): KrashExpressionOperator(first, second) {
 
-    override fun toValue(runtime: KrashRuntime) =
-        KrashValueInteger(first - second)
+    override fun invoke(first: KrashValueSimple, second: KrashValueSimple) = when(first) {
+
+        // Integer Division
+        is KrashValueInteger -> {
+
+            // Perform Division
+            if(second is KrashValueInteger) {
+
+                // Zero Safety
+                if(second.value == 0) throw RuntimeException("Cannot perform division by zero!")
+
+                // Perform Division
+                KrashValueInteger(first.value / second.value)
+            }
+
+            // Invalid Type
+            else throw RuntimeException("Invalid type to perform division!")
+        }
+
+        // Invalid Type
+        else -> throw RuntimeException("Invalid type to perform operator!")
+    }
+
+}
+
+class KrashExpressionOperatorMultiplication(first: KrashExpression, second: KrashExpression): KrashExpressionOperator(first, second) {
+
+    override fun invoke(first: KrashValueSimple, second: KrashValueSimple) = when(first) {
+
+        // Integer Multiplication
+        is KrashValueInteger -> {
+
+            // Perform Multiplication
+            if(second is KrashValueInteger) KrashValueInteger(first.value * second.value)
+
+            // Invalid Type
+            else throw RuntimeException("Invalid type to perform multiplication!")
+        }
+
+        // Invalid Type
+        else -> throw RuntimeException("Invalid type to perform operator!")
+    }
+
+}
+
+class KrashExpressionOperatorSubtraction(first: KrashExpression, second: KrashExpression): KrashExpressionOperator(first, second) {
+
+    override fun invoke(first: KrashValueSimple, second: KrashValueSimple) = when(first) {
+
+        // Integer Subtraction
+        is KrashValueInteger -> {
+
+            // Perform Subtraction
+            if(second is KrashValueInteger) KrashValueInteger(first.value - second.value)
+
+            // Invalid Type
+            else throw RuntimeException("Invalid type to perform subtraction!")
+        }
+
+        // Invalid Type
+        else -> throw RuntimeException("Invalid type to perform operator!")
+    }
 
 }
